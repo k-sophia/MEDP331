dd4b5312e6041e7c9b46714417f40f5bd245c145
Kelly Camacho <56777076+k-sophia@users.noreply.github.com>
2022-11-13 23:24:37 -0500

    Add files via upload

--- php/assignment-4.php
+++ php/assignment-4.php
@@ -7,58 +7,141 @@
         $dayOfWeek = $_POST['dayOfWeek'];
     }
 
-    $goal = '';
-    $urgent = '';
-    $task1 = '';
-    $task2 = '';
-    $task3 = '';
-    $task4 = '';
-    $task5 = '';
-    $appointments = '';
-    $notes = '';
+    $goal = array(
+        'get a head start on weeks assignments', 
+        'hang out with friends :)', 
+        'nothing', 
+        'try to make it till weekend', 
+        'take a long rest', 
+        'have fun XD', 
+        'get up early tomorrow'
+    );
 
-    if($dayOfWeek == 'monday') {
-        $goal = 'get a head start on weeks assignments';
-        $urgent = 'project due TOMORROW T^T';
-        $task1 = 'math hw';
-        $task2 = 'music readings';
-        $task3 = 'apply for internship';
-        $task4 = '';
-        $task5 = '';
-        $appointments = '3:45PM eye exam';
-        $notes = 'No Notes';
+    $urgent = array(
+        'project due TOMORROW T^T', 
+        'buy birthday gift for Nafi', 
+        'HACKATHON CHECK IN @ 2', 
+        'nothing :)', 
+        'exam @ 5', 
+        'nothing :)', 
+        'apply for graduation'
+    );
+
+    $task1 = array(
+        'math hw', 
+        'music readings', 
+        'study for exam', 
+        'pick up package', 
+        '', 
+        'meditate', 
+        ''
+    );
+    
+    $task2 = array(
+        'music readings', 
+        'update personal website', 
+        'bake for fund raiser', 
+        'grocery shopping', 
+        '', 
+        '', 
+        ''
+    );
+
+    $task3 = array(
+        'medp assignment', 
+        'capstone task', 
+        'clean fish tank', 
+        '', 
+        '', 
+        '', 
+        ''
+    );
+
+    $task4 = array(
+        'exercise', 
+        '', 
+        'interview prep', 
+        '', 
+        '', 
+        '', 
+        ''
+    );
+    $task5 = array(
+        '', 
+        '', 
+        '', 
+        '', 
+        '', 
+        '', 
+        ''
+    );
+
+    $appointments = array(
+        '3:45PM eye exam', 
+        '9AM club meeting <br> 4PM hackathon project meeting', 
+        'nothing :)', 
+        '5:45PM Fund Raiser', 
+        'nothing :)', 
+        '12:30PM dentist', 
+        '2PM escpae room <br> 6:30PM Birthday Party!!!! <br>'
+    );
+
+    $notes = array(
+        'No Notes', 
+        'bring identification to meeting', 
+        'nafi likes cats ^-^', 
+        'bring food labels to fund raiser', 
+        'No Notes', 
+        'No Notes', 
+        'reserve extra tickets for graduation'
+    );
+
+    $dayList = array('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
+    $num = array_search($dayOfWeek, $dayList);
+    if($num === false) {
+        echo <<<END
+            <div id="day-plan">
+                <div id="goal-container" class="container">
+                    <h3>INVALID INPUT</h3>
+                    <p>enter a valid week day</p>
+                </div>
+            </div>
+        END;
+    }
+    else {
+        echo <<<END
+            <h3 id="date">date: {$date}</h3>
+            <h3 id='dayOfWeek'>$dayOfWeek schedule</h3>
+            <div id="day-plan">
+                <div id="goal-container" class="container">
+                    <h3>GOAL FOR TODAY</h3>
+                    <p>$goal[$num]</p>
+                </div>
+                <div id="urgent-container" class="container">
+                    <h3>URGENT</h3>
+                    <p>$urgent[$num]</p>
+                </div>
+                <div id="tasks-container" class="container">
+                    <h3>TASK LIST</h3>
+                    <ul>
+                        <li>$task1[$num]</li>
+                        <li>$task2[$num]</li>
+                        <li>$task3[$num]</li>
+                        <li>$task4[$num]</li>
+                        <li>$task5[$num]</li>
+                    </ul>
+                </div>
+                <div id="appointments-container" class="container">
+                    <h3>APPOINTMENTS</h3>
+                    <p>$appointments[$num]</p>
+                </div>
+                <div id="notes-container" class="container">
+                    <h3>NOTES</h3>
+                    <p>$notes[$num]</p>
+                </div>
+            </div>
+        END;
     }
 
-    echo <<<END
-        <h3 id="date">DATE: {$date}</h3>
-        <h3 id='dayOfWeek'>DATE: $dayOfWeek Schedule</h3>
-        <div id="day-plan">
-            <div id="goal-container" class="container">
-                <h3>GOAL FOR TODAY</h3>
-                <p>$goal</p>
-            </div>
-            <div id="urgent-container" class="container">
-                <h3>URGENT</h3>
-                <p>$urgent</p>
-            </div>
-            <div id="tasks-container" class="container">
-                <h3>TASK LIST</h3>
-                <ul>
-                    <li>$task1</li>
-                    <li>$task2</li>
-                    <li>$task3</li>
-                    <li>$task4</li>
-                    <li>$task5</li>
-                </ul>
-            </div>
-            <div id="appointments-container" class="container">
-                <h3>APPOINTMENTS</h3>
-                <p>$appointments</p>
-            </div>
-            <div id="notes-container" class="container">
-                <h3>NOTES</h3>
-                <p>$notes</p>
-            </div>
-        </div>
-    END;
+
 ?>
